eaea3522631eb7b27a632262c0eda7e41f46e027
vinnieOrdobas <viniordobas@icloud.com>
2021-08-24 14:51:11 +0000

    changed file directory, created project

--- piratini_coffee_roasters/urls.py
+++ piratini_coffee_roasters/urls.py
@@ -1,4 +1,4 @@
-"""piratiniCoffeeRoasters URL Configuration
+"""piratini_coffee_roasters URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
--- piratini_coffee_roasters/wsgi.py
+++ piratini_coffee_roasters/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for piratiniCoffeeRoasters project.
+WSGI config for piratini_coffee_roasters project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'piratiniCoffeeRoasters.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'piratini_coffee_roasters.settings')
 
 application = get_wsgi_application()
