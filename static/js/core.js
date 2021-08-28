8788839ad096cce57e6603010b71cae8c749d80e
vinnieOrdobas <viniordobas@icloud.com>
2021-08-28 11:08:59 +0000

    finished navbar functionality

--- static/js/core.js
+++ static/js/core.js
@@ -1,4 +1,22 @@
 $(document).ready(function() {
     $(".dropdown-trigger").dropdown();
     $(".dropdown-trigger-bag").dropdown();
+    // search form toggle
+    $('.search-form').click(function() {
+        $('.search-form-wrapper').toggleClass('open');
+        $('.search-form-wrapper .search').focus();
+        $('html').toggleClass('search-form-open');
+      });
+      $('[data-toggle=search-form-close]').click(function() {
+        $('.search-form-wrapper').removeClass('open');
+        $('html').removeClass('search-form-open');
+      });
+    $('.search-form-wrapper .search').keypress(function( event ) {
+      if($(this).val() == "Search") $(this).val("");
+    });
+  
+    $('.search-close').click(function(event) {
+      $('.search-form-wrapper').removeClass('open');
+      $('html').removeClass('search-form-open');
+    });
 })
