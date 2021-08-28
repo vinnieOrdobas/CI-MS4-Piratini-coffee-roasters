599d20960546a0d3aa1c2502791d245a92ce4f3f
vinnieOrdobas <viniordobas@icloud.com>
2021-08-28 16:10:45 +0000

    Footer created for desktop and mobile

--- static/js/core.js
+++ static/js/core.js
@@ -6,6 +6,7 @@ $(document).ready(function() {
         edge: 'right',
         
     });
+    $('.modal').modal();
     // search form toggle
     $('.search-form').click(function() {
         $('.search-form-wrapper').toggleClass('open');
