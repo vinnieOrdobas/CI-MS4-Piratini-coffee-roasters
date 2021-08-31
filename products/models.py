885b9e4d099877cbcda3f9b3119fb33dfa407ee5
vinnieOrdobas <viniordobas@icloud.com>
2021-08-31 15:41:08 +0000

    Added products, created membership class

--- products/models.py
+++ products/models.py
@@ -20,7 +20,6 @@ class Product(models.Model):
     name = models.CharField(max_length=254)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    is_membership = models.BooleanField()
     # tags = models.ManyToManyField('tags', null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
@@ -32,10 +31,9 @@ class Product(models.Model):
         return self.description
 
 
-class Membership(models.Model):
-    product = models.ForeignKey('Product', on_delete=models.CASCADE)
-    # user = models.ForeignKey('User', on_delete=models.SET_NULL)
-    name = models.CharField(max_length=254)
+class Membership(Product):
+    # user = models.ForeignKey('User', on_delete=models.CASCADE)
+    is_membership = models.BooleanField(default=True)
 
     def __str__(self):
         return self.name
