d653daad0400157c24ede937475aa6e7ee78c981
vinnieOrdobas <viniordobas@icloud.com>
2021-09-04 11:12:20 +0000

    products app finished

--- products/views.py
+++ products/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product, Collection, Tags
+from .models import Product, Collection
+from django.db.models.functions import Lower
+
 # Create your views here.
 
 
