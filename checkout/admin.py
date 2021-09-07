51c40f852f90f6fee320cb1c4486d159f87a5dbd
vinnieOrdobas <viniordobas@icloud.com>
2021-09-07 16:14:11 +0000

    registered order, orderadmin, discountcode and discountcodeadmin

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from .models import DiscountCode, Order, OrderLineItem
 
-# Register your models here.
+
+class DiscountCodeAdmin(admin.ModelAdmin):
+    list_display = ('code', 'count', 'active', 'discount')
+    list_filter = ['active']
+    search_fields = ['code']
+
+
+class OrderLineItemAdmininline(admin.TabularInline):
+    model = OrderLineItem
+    readonly_fields = ('lineitem_total',)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (OrderLineItemAdmininline,)
+    readonly_fields = ('order_number', 'date',
+                            'delivery_cost', 'order_total',
+                            'grand_total',
+                        )
+    fields = ('order_number', 'date', 'full_name', 
+                'email', 'phone_number', 'country',
+                'postcode', 'town_or_city', 'street_address1',
+                'street_address2', 'county', 'delivery_cost',
+                'order_total', 'grand_total',
+                )
+    list_display = ('order_number', 'date', 'full_name',
+                     'order_total', 'delivery_cost', 'grand_total',)
+
+    ordering = ('-date',)
+
+
+admin.site.register(Order, OrderAdmin)
+admin.site.register(DiscountCode, DiscountCodeAdmin)
+
--- checkout/models.py
+++ checkout/models.py
@@ -2,16 +2,36 @@ import uuid
 
 from django.db import models
 from django.db.models import Sum
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.conf import settings
 
 from products.models import Product
+
+
+class DiscountCode(models.Model):
+    active = models.BooleanField(null=False, blank=False, default=True)
+    code = models.CharField(max_length=50, unique=True, null=False, blank=False)
+    count = models.IntegerField(null=False, blank=False, default=0)
+    discount = models.IntegerField(null=False, blank=False, validators=[MinValueValidator(0),
+                                                                        MaxValueValidator(100)])
+
+    def __str__(self):
+        return self.code
+
+    def increase_count(self):
+        if self.count < 10:
+            self.count += 1
+            super().save()
+        else:
+            self.active = False
+            super().save()
 
 
 class Order(models.Model):
     order_number = models.CharField(max_length=32, null=False, editable=False)
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
-    phone_number = models.CharField(max_length=40,null=False, blank=False)
+    phone_number = models.CharField(max_length=40, null=False, blank=False)
     country = models.CharField(max_length=40, null=False, blank=False)
     postcode = models.CharField(max_length=20, null=True, blank=True)
     town_or_city = models.CharField(max_length=40, null=False, blank=False)
@@ -19,6 +39,7 @@ class Order(models.Model):
     street_address2 = models.CharField(max_length=80, null=True, blank=True)
     county = models.CharField(max_length=80, null=True, blank=True)
     date = models.DateTimeField(auto_now_add=True)
+    # discount_code = models.ForeignKey(DiscountCode, null=True, blank=True, related_name='discount', on_delete=models.SET_NULL)
     delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
@@ -36,11 +57,12 @@ class Order(models.Model):
         if not self.order_number:
             self.order_number = self._generate_order_number()
         super().save(*args, **kwargs)
-    
+
     def update_total(self):
         """
         Update grand total each time a line item is added, accounting for delivery costs.
         """
+
         self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
         if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
             self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
@@ -48,7 +70,7 @@ class Order(models.Model):
             self.delivery_cost = 0
         self.grand_total = self.order_total + self.delivery_cost
         self.save()
-    
+
     def __str__(self):
         return self.order_number
 
@@ -57,7 +79,7 @@ class OrderLineItem(models.Model):
     order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
     product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
     quantity = models.IntegerField(null=False, blank=False, default=0)
-    lineitems_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
+    lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
 
     def save(self, *args, **kwargs):
         """
