6212961581c4bdcf512d9996a71013f933cbeaf5
vinnieOrdobas <viniordobas@icloud.com>
2021-09-10 17:22:21 +0000

    Revert "updated country field"
    This reverts commit a33c48b1e0978655c0d1e09b2796269ef6969bc1.

--- checkout/models.py
+++ checkout/models.py
@@ -4,8 +4,6 @@ from django.db import models
 from django.db.models import Sum
 from django.conf import settings
 
-from django_countries.fields import CountryField
-
 from products.models import Product
 
 
@@ -14,7 +12,7 @@ class Order(models.Model):
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     phone_number = models.CharField(max_length=40, null=False, blank=False)
-    country = CountryField(blank_label='', null=False, blank=False)
+    country = models.CharField(max_length=40, null=False, blank=False)
     postcode = models.CharField(max_length=20, null=True, blank=True)
     town_or_city = models.CharField(max_length=40, null=False, blank=False)
     street_address1 = models.CharField(max_length=80, null=False, blank=False)
