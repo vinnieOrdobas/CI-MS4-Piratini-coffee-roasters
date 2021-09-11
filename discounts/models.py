fa07682209bd35549359fbf3c37c9eab811688bc
vinnieOrdobas <viniordobas@icloud.com>
2021-09-11 09:34:38 +0000

    created discount app and migrate models

--- discounts/models.py
+++ discounts/models.py
@@ -12,4 +12,3 @@ class Discount(models.Model):
 
     def __str__(self):
         return self.code
-
--- piratini_coffee_roasters/settings.py
+++ piratini_coffee_roasters/settings.py
@@ -58,6 +58,7 @@ INSTALLED_APPS = [
     'products',
     'bag',
     'checkout',
+    'discounts',
 
     # Other apps
     'materializecssform',
