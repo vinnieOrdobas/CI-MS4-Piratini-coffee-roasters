0dfd81b58fda108a3b4ea7893bdbd32893b9d7b5
vinnieOrdobas <viniordobas@icloud.com>
2021-09-13 16:30:49 +0000

    updated profile template

--- profiles/views.py
+++ profiles/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 
 from .models import UserProfile
 from .forms import UserProfileForm
+
+from checkout.models import Order
 
 def profile(request):
     """
@@ -10,6 +13,12 @@ def profile(request):
 
     profile = get_object_or_404(UserProfile, user=request.user)
 
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile updated successfully')
+
     orders = profile.orders.all()
     form = UserProfileForm(instance=profile)
 
@@ -17,6 +26,23 @@ def profile(request):
     context = {
         'orders': orders,
         'form': form,
+    }
+
+    return render(request, template, context)
+
+
+def order_history(request, order_number):
+    order = get_object_or_404(Order, order_number=order_number)
+
+    messages.info(request, (
+        f'This is a past confirmation for order number {order_number}.'
+        'A confirmation email was sent on the order date.'
+    ))
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order':order,
+        'from_profile': True,
     }
 
     return render(request, template, context)
