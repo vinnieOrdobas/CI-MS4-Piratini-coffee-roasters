b5138852808b25a3c772ed7cfb080f30c0540d28
vinnieOrdobas <viniordobas@icloud.com>
2021-09-14 09:31:22 +0000

    fixed minor bug with different operand types in the order model in the checkout app

--- checkout/models.py
+++ checkout/models.py
@@ -68,7 +68,7 @@ class Order(models.Model):
 
         if self.discount:
             discount_percentage = self.discount.discount
-            discount_value = self.order_total * Decimal(discount_percentage / 100)
+            discount_value = self.order_total * (discount_percentage / 100)
         else:
             discount_value = 0
 
