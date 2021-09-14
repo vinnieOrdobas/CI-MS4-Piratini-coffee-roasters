b5d57b3159cc4658fd3193d585936ceaefd84025
vinnieOrdobas <viniordobas@icloud.com>
2021-09-14 09:49:41 +0000

    fixed minor bug on order model and discount code

--- checkout/views.py
+++ checkout/views.py
@@ -47,7 +47,7 @@ def checkout(request):
 
         else:
             discount = None
-        
+
         form_data = {
             'full_name': request.POST['full_name'],
             'email': request.POST['email'],
@@ -155,7 +155,7 @@ def checkout_success(request, order_number):
 
         if save_info:
             profile_data = {
-                'default_phone_number': order_phone_number,
+                'default_phone_number': order.phone_number,
                 'default_country': order.country,
                 'default_postcode': order.postcode,
                 'default_town_or_city': order.town_or_city,
@@ -166,8 +166,6 @@ def checkout_success(request, order_number):
             user_profile_form = UserProfileForm(profile_data, instance=profile)
             if user_profile_form.is_valid():
                 user_profile_form.save()
-
-
 
     messages.success(request, f'Order successfully processed! \
         A confirmation email will be sent to {order.email}.')
