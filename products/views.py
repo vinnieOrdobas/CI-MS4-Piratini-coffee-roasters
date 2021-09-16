bb338ed74c9b6791c07bd1eb971d34774b808980
vinnieOrdobas <viniordobas@icloud.com>
2021-09-16 06:59:16 +0000

    started product management template

--- products/views.py
+++ products/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
 from .models import Product, Collection
+from .forms import ProductForm
 from django.db.models.functions import Lower
 
 from profiles.models import UserProfile
@@ -83,3 +84,14 @@ def product_detail(request, product_id):
 
     return render(request, 'products/product_detail.html', context)
 
+
+def add_product(request):
+    """
+    Add a product to the store
+    """
+    form = ProductForm()
+    template = 'products/add_product.html'
+    context = {
+        'form': form,
+    }
+    return render(request, template, context)
