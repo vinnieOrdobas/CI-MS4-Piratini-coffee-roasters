bf725e38d7bb2b4abf6763cb47be0af14e5a3c28
vinnieOrdobas <viniordobas@icloud.com>
2021-09-19 18:06:38 +0000

    fixed database URL

--- piratini_coffee_roasters/settings.py
+++ piratini_coffee_roasters/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
@@ -132,7 +132,7 @@ WSGI_APPLICATION = 'piratini_coffee_roasters.wsgi.application'
 
 if 'DATABASE_URL' in os.environ:
     DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE)URL'))
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
 else:
     DATABASES = {
